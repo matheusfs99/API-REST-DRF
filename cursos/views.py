7c4ce5cef1ff02c7c44f284e1cb36bddb47595db
Matheus Farias <matheusfarias009@hotmail.com>
2020-08-22 01:58:08 -0300

    Criação dos métodos GET e rota para acessá-los

--- cursos/views.py
+++ cursos/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
 
-# Create your views here.
+class CursoAPIView(APIView):
+    """
+    API de Cursos da Geek University
+    """
+    def get(self, request):
+        cursos = Curso.objects.all()
+        serializer = CursoSerializer(cursos, many=True)
+        return Response(serializer.data)
+
+class AvaliacaoAPIView(APIView):
+    """
+    API de Avaliações da Geek University
+    """
+    def get(self, request):
+        avaliacoes = Avaliacao.objects.all()
+        serializer = AvaliacaoSerializer(avaliacoes, many=True)
+        return Response(serializer.data)
+
