c11350b1a535905ef9de9e063451c1f08def6623
Matheus Farias <matheusfarias009@hotmail.com>
2020-09-10 16:41:30 -0300

    Adicionado campo de média de avaliações por curso com função no serializer

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from .models import Curso, Avaliacao
 
@@ -20,7 +21,7 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
 
     def validate_avaliacao(self, valor):
         if valor in range(1, 6):
-            return True
+            return valor
         raise serializers.ValidationError('A avaliação precisa ser um inteiro entre 1 e 5')
 
 class CursoSerializer(serializers.ModelSerializer):
@@ -35,6 +36,8 @@ class CursoSerializer(serializers.ModelSerializer):
     # Primary Key Related Field
     avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
+    media_avaliacoes = serializers.SerializerMethodField()
+
     class Meta:
         model = Curso
         fields = (
@@ -43,5 +46,13 @@ class CursoSerializer(serializers.ModelSerializer):
             'url',
             'criacao',
             'ativo',
-            'avaliacoes'
+            'avaliacoes',
+            'media_avaliacoes'
         )
+
+    def get_media_avaliacoes(self, obj):
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+
+        if media is None:
+            return 0
+        return round(media * 2) / 2
